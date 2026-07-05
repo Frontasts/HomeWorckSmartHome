9d1524b263182e8a926638e2987c54b9b06c9628
Frontasts <steamfronts0110@gmail.com>
2026-07-05 15:08:32 +0300

    Класс SmartHome вынесен в отдельный фаил. Был убран метод GetStatus(), все выводы у методов были переделанны через bool связи с чем была изменена логика у сетеров, были убранны не нужные проверки и упращенна логика.

---
+++ HomeWorckSmartHome/SmartHome.cs
@@ -0,0 +1,104 @@
+﻿namespace HomeWorckSmartHome
+{
+    public class SmartHome
+    {
+        private bool _lightIsOn;
+        private bool _heatingIsOn;
+        private int _temperature;
+        private string _currentRoom;
+
+        public SmartHome()
+        {
+            _lightIsOn = false;
+            _heatingIsOn = false;
+            _temperature = 20;
+            _currentRoom = "Гостиная";
+        }
+
+        public bool TurnOnLight()
+        {
+            if (_lightIsOn)
+            {
+                return false;
+            }
+
+            _lightIsOn = true;
+            return true;
+        }
+
+        public bool TurnOffLight()
+        {
+            if (_lightIsOn == false)
+            {
+                return false;
+            }
+
+            _lightIsOn = false;
+            return true;
+        }
+
+        public bool TurnOnHeating()
+        {
+            if (_heatingIsOn)
+            {
+                return false;
+            }
+
+            _heatingIsOn = true;
+            return true;
+        }
+
+        public bool TurnOffHeating()
+        {
+            if (_heatingIsOn == false)
+            {
+                return false;
+            }
+
+            _heatingIsOn = false;
+            return true;
+        }
+
+        public bool TrySetTemperature(int newTemperature)
+        {
+            if (newTemperature < 10 || newTemperature > 35)
+            {
+                return false;
+            }
+
+            _temperature = newTemperature;
+            return true;
+        }
+
+        public bool TrySetRoom(string roomName)
+        {
+            if (string.IsNullOrWhiteSpace(roomName))
+            {
+                return false;
+            }
+
+            _currentRoom = roomName;
+            return true;
+        }
+
+        public bool GetLightStatus()
+        {
+            return _lightIsOn;
+        }
+
+        public bool GetHeatingStatus()
+        {
+            return _heatingIsOn;
+        }
+
+        public int GetTemperature()
+        {
+            return _temperature;
+        }
+
+        public string GetCurrentRoom()
+        {
+            return _currentRoom;
+        }
+    }
+}
